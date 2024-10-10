2991aa885f73ec774a15b78a54f4c37f5bd22fe9
Grizenzio Orchivillando <141236550+orchivillando@users.noreply.github.com>
2024-10-10 18:44:11 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Simple PWA in One File</title>
+  <title>Festivak Transformasi Pendidikan</title>
   <link rel="apple-touch-icon" sizes="180x180" href="https://i.ibb.co.com/tmK6GSZ/apple-touch-icon.png">
   <link rel="icon" type="image/png" sizes="32x32" href="https://i.ibb.co.com/F7TzwCP/favicon-32x32.png">
   <link rel="icon" type="image/png" sizes="16x16" href="https://i.ibb.co.com/D70CCD8/favicon-16x16.png">
